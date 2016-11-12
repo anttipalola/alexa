b4b120fc5630020de2fbdb2db46a10ef6dd42c4e
Antti Palola <antti.palola@vincit.fi>
2016-11-12 17:12:49 +0000

    Fix serializer
    * rename guid to uid
    * add redirection URL
    * added psycopg2 requirement

--- yle/serializers.py
+++ yle/serializers.py
@@ -1,18 +1,24 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer, CharField, UUIDField
 
+from alexa.settings import RADIO_LINK_BASE
 from yle.models import News
 
 
 class NewsSerializer(ModelSerializer):
 
-    guid = UUIDField(source='uuid')
+    uid = UUIDField(source='uuid')
     updateDate = CharField(source='modified')
     titleText = CharField(source='title')
     streamUrl = CharField(source='audio_url')
     mainText = CharField(source='content')
+    redirectionUrl = SerializerMethodField('get_redirection')
+
+    def get_redirection(self, obj):
+        return RADIO_LINK_BASE + obj.external_id
 
     class Meta:
         model = News
-        fields = ['guid', 'updateDate',
+        fields = ['uid', 'updateDate',
                   'titleText', 'streamUrl',
-                  'mainText']
+                  'mainText', 'redirectionUrl']
--- yle/tests.py
+++ yle/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 
-# Create your tests here.
 from yle.models import News
 from yle.tasks import fetch_news, fetch_audio_url
 
@@ -19,3 +18,19 @@ class YleTests(TestCase):
         n.refresh_from_db()
         # There's at least some url with an mp3 file.
         self.assertRegex(n.audio_url, r'http.?:\/\/.*\.mp3')
+
+    def test_serializer(self):
+        n = News.objects.create(external_id='123',
+                                audio_url='http://url.mp3',
+                                title='The title',
+                                created='2014-01-01T14:00:00Z')
+        res = self.client.get('/news/')
+        self.assertEqual(res.status_code, 200)
+
+        data = res.json()
+        print(data)
+        self.assertEqual(len(data), 1)
+        news = data[0]
+        self.assertEqual(str(n.uuid), news['uid'])
+
+
